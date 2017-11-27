3a2ed15fb1785b1e93c3fb55fb7d6d570ebcd021
inioluwa adebanjo <adeb6600@gmail.com>
2017-11-27 15:37:58 +0100

    Update rave.card.tokencharge.js

--- services/rave.card.tokencharge.js
+++ services/rave.card.tokencharge.js
@@ -1,10 +1,10 @@
 var morx = require('morx');
 var q = require('q');
-var charge = require('./rave.tokencharge');
 
 var spec =  morx.spec()
-			.build('currency', 'required:false, eg:NGN')
+				.build('currency', 'required:false, eg:NGN')
 				.build('country', 'required:false, eg:NG')
+				.build('seckey', 'required:true, eg:FLWSECK-4f215bded21389132232dc78fdfb207e-X')
 				.build('token', 'required:true, {“user_token”:“f0209”,“embed_token”:“flw-t0-9f3aa69a806f6440fbb78cc9e8b2f135-k3n”}')
 				.build('amount', 'required:true, eg:10') 
 				.build('email', 'required:true, eg:debowalefaulkner@gmail.com')
@@ -20,26 +20,53 @@ var spec =  morx.spec()
 function service(data, _rave){
 
 	var d = q.defer();
+
 	q.fcall( () => {
 
 		var validated = morx.validate(data, spec, _rave.MORX_DEFAULT);
 		var params = validated.params;
 
-		return charge(params, _rave);
+		return params;
 
 	})
-	.then( resp => {
+	.then( params  => {
 
-		d.resolve(resp);
+
+		if(params.include_integrity_hash){
+
+			delete params.include_integrity_hash;
+			var integrity_hash = _rave.getIntegrityHash(params, _rave.getPublicKey(), _rave.getSecretKey());
+			params.QUERY_STRING_DATA = JSON.parse(JSON.stringify(params));
+			params.QUERY_STRING_DATA.integrity_hash = integrity_hash;
+
+			//console.log(params);
+			
+			
+		}
+		
+		var encrypted = _rave.encrypt(params);
+		var payload = {};
+		payload.PBFPubKey = _rave.getPublicKey();
+		payload.client = encrypted;
+		payload.alg = '3DES-24';
+		
+		return _rave.request('flwv3-pug/getpaidx/api/tokenized/charge', payload)
+	})
+	.then( response => {
+
+		//console.log(response);
+		d.resolve(response);
 
 	})
 	.catch( err => {
 
 		d.reject(err);
 
-	});
+	})
 
 	return d.promise;
+	
+	
 
 }
 service.morxspc = spec;
